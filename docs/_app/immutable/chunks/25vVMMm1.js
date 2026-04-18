c5441cd869726beb22b5ee406bd99c8cb5d79342
Abishek P <abishek.atp@gmail.com>
2026-04-19 00:07:14 +0530

    build: what is the purpose of my life

---
+++ docs/_app/immutable/chunks/25vVMMm1.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1c4pdma?.base??"/abishekatp",a=globalThis.__sveltekit_1c4pdma?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/DOqlpz4R.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_flt2gk?.base??"/abishekatp",t=globalThis.__sveltekit_flt2gk?.assets??s??"";export{t as a,s as b};

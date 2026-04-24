898b6f5691a9a8db883c460780fceee48a1b1a12
Abishek P <abishek.atp@gmail.com>
2026-04-24 23:37:31 +0530

    build: escapism - slow poison or healing medicine

--- docs/_app/immutable/chunks/B0IkdbiH.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_sh2ohk?.base??"/abishekatp",a=globalThis.__sveltekit_sh2ohk?.assets??s??"";export{a,s as b};
---
+++ docs/_app/immutable/chunks/DmfxwAHj.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_hfpos6?.base??"/abishekatp",a=globalThis.__sveltekit_hfpos6?.assets??s??"";export{a,s as b};

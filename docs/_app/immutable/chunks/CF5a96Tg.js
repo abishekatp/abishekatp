7c01982f862604929ef0c00b61b88a3da9039ffb
Abishek P <abishek.atp@gmail.com>
2026-05-10 12:29:07 +0530

    build: colorism - the fifty shades of brown

---
+++ docs/_app/immutable/chunks/CF5a96Tg.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1ktres1?.base??"/abishekatp",e=globalThis.__sveltekit_1ktres1?.assets??s??"";export{e as a,s as b};
--- docs/_app/immutable/chunks/CJytZqEL.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_4yri1m?.base??"/abishekatp",a=globalThis.__sveltekit_4yri1m?.assets??s??"";export{a,s as b};

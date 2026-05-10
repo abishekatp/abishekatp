7931d7493723f0a1fe5cd33f1dad7c323a05a7fb
Abishek P <abishek.atp@gmail.com>
2026-05-10 12:54:25 +0530

    build: color - the fiftey shades of brown

---
+++ docs/_app/immutable/chunks/BWap-RfD.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_4gggin?.base??"/abishekatp",a=globalThis.__sveltekit_4gggin?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/CF5a96Tg.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1ktres1?.base??"/abishekatp",e=globalThis.__sveltekit_1ktres1?.assets??s??"";export{e as a,s as b};

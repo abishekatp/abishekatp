2d1c3821dad2c0ba35580b225c38dff6b2baa6f7
Abishek P <abishek.atp@gmail.com>
2026-05-23 02:12:34 +0530

    build: the city - resources and opportunities, fictions tell more than stores

---
+++ docs/_app/immutable/chunks/BtZvHYhD.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1n3cjlj?.base??"/abishekatp",a=globalThis.__sveltekit_1n3cjlj?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/PPXmo1fP.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1ksfz6h?.base??"/abishekatp",a=globalThis.__sveltekit_1ksfz6h?.assets??s??"";export{a,s as b};

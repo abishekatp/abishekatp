1844895f7228f45923469373c0126b1684685209
Abishek P <abishek.atp@gmail.com>
2026-05-23 17:28:45 +0530

    build: fix the city resources and opportunities

--- docs/_app/immutable/chunks/BtZvHYhD.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1n3cjlj?.base??"/abishekatp",a=globalThis.__sveltekit_1n3cjlj?.assets??s??"";export{a,s as b};
---
+++ docs/_app/immutable/chunks/xshGZYOs.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1furtrm?.base??"/abishekatp",t=globalThis.__sveltekit_1furtrm?.assets??s??"";export{t as a,s as b};

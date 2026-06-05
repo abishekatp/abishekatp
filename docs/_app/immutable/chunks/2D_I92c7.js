c07e09fa174eaa7b99d0b07b86034038da427c75
Abishek P <abishek.atp@gmail.com>
2026-06-05 23:45:56 +0530

    build: listening to the language of the world

--- docs/_app/immutable/chunks/2D_I92c7.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_uc2bv7?.base??"/abishekatp",a=globalThis.__sveltekit_uc2bv7?.assets??s??"";export{a,s as b};
---
+++ docs/_app/immutable/chunks/Bc6Mw6Cc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1b7jrso?.base??"/abishekatp",a=globalThis.__sveltekit_1b7jrso?.assets??s??"";export{a,s as b};
